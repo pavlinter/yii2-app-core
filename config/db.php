eef5e0879572ed81789b0cc827e7543c0da992ed
Pavel <pavlinter@gmail.com>
2015-02-10 10:16:57 +0200

    .

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=OwnDbName',
+    'dsn' => 'mysql:host=mysql;dbname=OwnDbName',
     'username' => '',
     'password' => '',
     'charset' => 'utf8',
