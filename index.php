179b2086a457b5ebacca86ab14df6070b1e2a2f3
Pavel <pavlinter@gmail.com>
2015-08-11 10:12:33 +0300

    Enable debug

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if($_SERVER['SERVER_ADDR'] == '127.0.0.1') {
         require(__DIR__ . '/config/web-local.php')
     );
 } else {
-    defined('YII_DEBUG') or define('YII_DEBUG', false);
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
     defined('YII_ENV') or define('YII_ENV', 'prod');
 
     require(__DIR__ . '/vendor/autoload.php');
